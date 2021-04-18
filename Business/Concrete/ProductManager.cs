ab1c4ddcedc589d9843a19c085348f7d929015d8
kenanc66 <kenancicekdev@gmail.com>
2021-04-19 02:00:07 +0300

    maintenance time

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -60,7 +60,7 @@ namespace Business.Concrete
         [CacheAspect] //key,value
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 1)
+            if (DateTime.Now.Hour == 5)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
